6c0612f9133146dca6abe1e4d3d215931f0764c1
Pelochus <anghflores@gmail.com>
2024-08-12 11:33:56 +0200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # Title: Installation script
 # Author: Pelochus
 # Date: 2024-08-04
+# Version: 1.1
 # Description: Installation script for pprz-py-plotter on Ubuntu/Debian
 
 # Quit on error
@@ -23,13 +24,25 @@ install_packages() {
 }
 
 main() {
+    info "This script will install pprz-py-plotter on your Ubuntu/Debian machine."
+    info "The repository for this project will be downloaded on your current directory."
+    info "If you want to install it in another directory, please cd into that directory before running this script."
+    info "Do you want to continue? (y/n)"
+
+    read -r response
+    
+    if [ "$response" != "y" ]; then
+        info "Installation aborted!"
+        exit 1
+    fi
+    
     update_system
     install_packages
 
     git clone https://github.com/Swarm-Systems-Lab/pprz-py-plotter.git
 
     info "Installation completed!"
-    info "Change into pprz-py-plotter dir and run GUI with ./pprz-py-plotter"
+    info "cd into pprz-py-plotter directory and run GUI with ./pprz-py-plotter"
 }
 
 # Run!
